0e051d5313ff91d8db7acdc2dfcfa9308a9d780f
Oke Amuwha <amuwhao46@gmail.com>
2023-04-07 13:48:21 -0400

    static template version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
+import Container from "./components/Container";
+import Input from "./components/Input";
+import Summary from "./components/Summary/Summary";
+import Tasks from "./components/Tasks/Tasks";
 import './index.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center m-5">
+      <div className="flex flex-col items-center">
+        <div className="sm:w-[640px] border shadow p-10 flex flex-col gap-10">
+          <Container title={"Summary"}>
+            <Summary />
+          </Container>
+          <Container>
+            <Input />
+          </Container>
+          <Container title={"Tasks"}>
+            <Tasks />
+          </Container>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-type Props = {}
-
-function Container({}: Props) {
+const Container = ({
+  children,
+  title,
+}: {
+  children: JSX.Element | JSX.Element[];
+  title?: string;
+}) => {
   return (
-    <div>Container</div>
-  )
-}
+    <div className="bg-green-600 p-4 border shadow rounded-md">
+      {title && <h2 className="text-xl pb-2 text-white">{title}</h2>}
+      <div>{children}</div>
+    </div>
+  );
+};
 
-export default Container
+export default Container;
